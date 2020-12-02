1d9b016e711a3c2e4a22dbc7f748de86727b6f2e
LitatyHQ <litatyHQ@gmail.com>
2020-12-01 23:02:07 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 7, "M", "brown", "joyful", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 7, "M", "black", "playful", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 10, "F", "white", "protective", 1);
+INSERT INTO bears VALUES (4, "Sereant Brown", 15, "F", "black", "strict", 1);
+INSERT INTO bears VALUES (5, "Melissa", 2, "F", "white", "playful", 1);
+INSERT INTO bears VALUES (6, "Grinch", 12, "M", "black", "mean", 1);
+INSERT INTO bears VALUES (7, "Wendy", 3, "F", "cinnamon", "sensitive", 1);
+INSERT INTO bears VALUES (8, null, 12, "F", "cinnamon", "attentive", 0);
